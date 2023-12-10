7c1091c7d318ca71a9011d385f1eea5f08d60813
Abdussalam-Mujeeb-ur-rahman <mjbabdussalam@gmail.com>
2023-12-10 02:33:14 +0100

    signup, login and logout done ✅

---
+++ backend/v1/utils/validators.js
@@ -0,0 +1,54 @@
+const { body, validationResult } = require("express-validator");
+
+const validate = (validations) => {
+  return async (req, res, next) => {
+    try {
+      // Ensure that validations is an array
+      if (!Array.isArray(validations)) {
+        throw new Error("Validations must be an array.");
+      }
+
+      // Execute each validation and collect the results
+      const validationResults = await Promise.all(validations.map((validation) => validation.run(req)));
+
+      // Check if any validation failed
+      const errors = validationResults.reduce((acc, result) => {
+        if (!result.isEmpty()) {
+          return acc.concat(result.array());
+        }
+        return acc;
+      }, []);
+
+      // If there are errors, respond with a 422 status and error details
+      if (errors.length > 0) {
+        return res.status(422).json({ success: false, error: errors[0].msg });
+      }
+
+      // If validations pass, proceed to the next middleware
+      return next();
+    } catch (error) {
+      // Handle unexpected errors
+      console.error("Validation error:", error.message);
+      return res.status(500).json({ success: false, error: "Please make sure you are sending the expected request body. Try again later! :)" });
+    }
+  };
+};
+
+const loginValidator = [
+  body("email").trim().isEmail().withMessage("Email is required"),
+  body("password").trim().isLength({ min: 6 }).withMessage("Password should contain at least 6 characters!"),
+];
+
+const signupValidator = [
+  body("name").notEmpty().withMessage("Name is required"),
+  body("firstName"),
+  body("lastName"),
+  body("phoneNumber"),
+  ...loginValidator,
+];
+
+module.exports = {
+  loginValidator,
+  signupValidator,
+  validate,
+};
